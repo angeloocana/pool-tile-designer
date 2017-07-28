3f9554f478dac9d61a0c8b5cacdf1a8d04640f50
Angelo Ocana <angeloocana@gmail.com>
2017-07-28 01:03:08 +0000

    Fix invisible import

--- src/tiles/components/Color.js
+++ src/tiles/components/Color.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { InvisibleSpan } from '../../shared/components/Invisible';
+import { InvisibleSpan } from '../../core/components/Invisible';
 
 const Input = styled.input`
   appearance: none;
